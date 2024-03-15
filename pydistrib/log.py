39bf4d1ef33d0accd10b6ef24c6ba9fa86e05c93
Tazmilur Saad <ssaad@colgate.edu>
2024-03-15 17:44:15 -0400

    Added removal of stale segments.

--- pydistrib/log.py
+++ pydistrib/log.py
@@ -2,6 +2,7 @@ from time import clock_gettime_ns, CLOCK_REALTIME
 
 import io
 import ujson
+import os
 
 
 class Entry[T]:
@@ -25,31 +26,45 @@ class Entry[T]:
 class Log[T]:
     fp: io.TextIOWrapper
     index: int
-    offset: int
+    base: int
+    base_low: int
     max_size: int
     filename: str
 
     def __init__(self, filename: str, max_size: int = 1024) -> None:
         self.filename = filename
-        self.offset = 0
-        self.fp = open(file=f"{self.filename}_{self.offset}", mode="a", encoding="UTF-8")
+        self.base = 0
+        self.base_low = 0
+        self.fp = open(file=f"{self.filename}_{self.base}", mode="a", encoding="UTF-8")
         self.index = 0
         self.max_size = max_size
 
-
     def _maybe_roll(self) -> None:
-        if (new_offset := self.index // self.max_size) <= self.offset:
+        if (new_offset := self.index // self.max_size) <= self.base:
             return
 
-        self.offset = new_offset
+        self.base = new_offset
         self.fp.flush()
         self.fp.close()
-        self.fp = open(file=f"{self.filename}_{self.offset}", mode="a", encoding="UTF-8")
+        self.fp = open(file=f"{self.filename}_{self.base}", mode="a", encoding="UTF-8")
 
     def _convert(self, value: T) -> Entry[T]:
         self._maybe_roll()
         self.index += 1
         return Entry(value=value, index=self.index)
+
+    def _maybe_discard_logs(self) -> None:
+        if abs(self.base - self.base_low) >= 5:
+            self.base_low = self.base
+            self._discard_logs()
+
+    def _discard_logs(self) -> None:
+
+        candidates = [file for file in os.listdir() if self.filename in file]
+        candidates = [file for file in candidates if int(file.split("_")[-1]) < self.base_low]
+
+        for file in candidates:
+            os.remove(file)
 
     def write(self, value: T) -> None:
         return self._persist_batch(entries=[self._convert(value)])
@@ -61,3 +76,6 @@ class Log[T]:
         for entry in entries:
             ujson.dump(entry.json(), self.fp)
             self.fp.write("\n")
+
+        self.fp.flush()
+        self._maybe_discard_logs()
